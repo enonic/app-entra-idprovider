211a6f5a45e8b8d5079ae80fbab2b37f58c8b47f
Anatol Sialitski <asi@enonic.com>
2025-11-20 10:11:09 +0100

    Entra IDP Migration Guide #8

--- src/test/resources/lib/configFile/configIdProvider-test.js
+++ src/test/resources/lib/configFile/configIdProvider-test.js
@@ -123,7 +123,7 @@ exports.testValidConfig = () => {
     test.assertEquals(2, config.acceptLeeway);
     test.assertJsonEquals([{groupProperty: 'id', regexp: 'tid1', and: 'false'}], config.groupFilter);
     test.assertTrue(config.createAndUpdateGroupsOnLoginFromGraphApi);
-    test.assertEquals('azure-ad-', config.groupPrefix);
+    test.assertEquals('azure-ad', config.groupPrefix);
     test.assertJsonEquals(['tenant1', 'tenant2'], config.allowedTenants);
 };
 
@@ -177,10 +177,10 @@ exports.testDefaultConfigWithRequiredOptions = () => {
     test.assertJsonEquals([], config.autoLogin.allowedAudience);
 
     test.assertEquals(1, config.acceptLeeway);
-    test.assertJsonEquals([], config.groupFilter);
+    test.assertNull(config.groupFilter);
     test.assertNull(config.pageSize);
     test.assertFalse(config.createAndUpdateGroupsOnLoginFromGraphApi);
-    test.assertEquals('azure-ad-', config.groupPrefix);
+    test.assertEquals('azure-ad', config.groupPrefix);
     test.assertJsonEquals([], config.allowedTenants);
 };
 
